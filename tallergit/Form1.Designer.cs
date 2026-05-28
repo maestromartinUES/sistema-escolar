579cef77f3adf721d7cab1d328a619ba655ca90f
Martin Anduaga <martinanduaga9@gmail.com>
2026-05-28 10:43:57 -0700

    Agregar logo ues

--- tallergit/Form1.Designer.cs
+++ tallergit/Form1.Designer.cs
@@ -28,12 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             button1 = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(269, 223);
+            button1.Location = new Point(279, 270);
             button1.Name = "button1";
             button1.Size = new Size(234, 97);
             button1.TabIndex = 0;
@@ -41,19 +44,31 @@
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(285, 187);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox1);
             Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
         private Button button1;
+        private PictureBox pictureBox1;
     }
 }
--- tallergit/obj/Debug/net8.0-windows/tallergit.AssemblyInfo.cs
+++ tallergit/obj/Debug/net8.0-windows/tallergit.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tallergit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3e8fb90fa3d57f96f4510d15e10ab31a29b53b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5fdf5cf2bb53df54965a4ab1a50f37ca042572e")]
 [assembly: System.Reflection.AssemblyProductAttribute("tallergit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tallergit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
